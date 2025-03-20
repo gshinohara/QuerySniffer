ede370d72867125bbf75bd24c1c10042141aeb66
Gaku Shinohara <gaku.shinohara@vicc.jp>
2025-03-20 15:35:34 +0900

    Add simple implementation

---
+++ QuerySniffer/Components/Attributes/QueryObjectAttributes.cs
@@ -0,0 +1,11 @@
+﻿using Grasshopper.Kernel.Attributes;
+
+namespace QuerySniffer.Components.Attributes
+{
+    internal class QueryObjectAttributes : GH_FloatingParamAttributes
+    {
+        public QueryObjectAttributes(QueryObject owner) : base(owner)
+        {
+        }
+    }
+}
---
+++ QuerySniffer/Components/QueryObject.cs
@@ -0,0 +1,21 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using QuerySniffer.Components.Attributes;
+using System;
+
+namespace QuerySniffer.Components
+{
+    public class QueryObject : GH_Param<IGH_Goo>
+    {
+        public QueryObject() : base("Query Object", "QO", "Query Object", "Params", "Util", GH_ParamAccess.item)
+        {
+        }
+
+        public override Guid ComponentGuid => new Guid("ECE7231E-02C9-4352-AA18-86B193F92361");
+
+        public override void CreateAttributes()
+        {
+            Attributes = new QueryObjectAttributes(this);
+        }
+    }
+}
