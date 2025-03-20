16abe19f585041cc3914b9250be4729f5534f6fc
Gaku Shinohara <gaku.shinohara@vicc.jp>
2025-03-20 16:58:26 +0900

    Implement rendering feature of custom input grip

--- QuerySniffer/Components/Attributes/QueryObjectAttributes.cs
+++ QuerySniffer/Components/Attributes/QueryObjectAttributes.cs
@@ -1,4 +1,6 @@
-﻿using Grasshopper.Kernel.Attributes;
+﻿using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel.Attributes;
+using System.Drawing;
 
 namespace QuerySniffer.Components.Attributes
 {
@@ -6,6 +8,33 @@ namespace QuerySniffer.Components.Attributes
     {
         public QueryObjectAttributes(QueryObject owner) : base(owner)
         {
+        }
+
+        public override bool HasInputGrip => false;
+
+        private PointF CustomInputGrip => Pivot + new SizeF(Bounds.Width * -0.2f, Bounds.Height / 2);
+
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+            switch (channel)
+            {
+                case GH_CanvasChannel.Objects:
+                    GH_Capsule capsule = IsIconMode(Owner.IconDisplayMode) ?
+                        GH_Capsule.CreateCapsule(Bounds, GH_CapsuleRenderEngine.GetImpliedPalette(Owner)) :
+                        GH_Capsule.CreateTextCapsule(Bounds, Bounds, GH_CapsuleRenderEngine.GetImpliedPalette(Owner), Owner.NickName);
+
+                    capsule.AddInputGrip(CustomInputGrip);
+                    capsule.AddOutputGrip(OutputGrip.Y);
+
+                    if (IsIconMode(Owner.IconDisplayMode))
+                        capsule.Render(graphics, icon: Owner.Locked ? Owner.Icon_24x24_Locked : Owner.Icon_24x24, selected: Selected, locked: Owner.Locked, hidden: false);
+                    else
+                        capsule.Render(graphics, selected: Selected, locked: Owner.Locked, hidden: false);
+
+                    capsule.Dispose();
+
+                    break;
+            }
         }
     }
 }
