70b44d412151a12f015e08e31c8dc8ae7d27d513
Gaku Shinohara <gaku.shinohara@vicc.jp>
2025-03-24 14:36:53 +0900

    Implement wire behavior

--- QuerySniffer/Components/Interactions/QueryObjectInteraction.cs
+++ QuerySniffer/Components/Interactions/QueryObjectInteraction.cs
@@ -7,13 +7,22 @@ using QuerySniffer.Components.Attributes;
 using QuerySniffer.Types;
 using System.Drawing;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace QuerySniffer.Components.Interactions
 {
     internal class QueryObjectInteraction : GH_AbstractInteraction
     {
+        private enum WireState
+        {
+            New,
+            Add,
+            Remove,
+        }
+
         private QueryObject m_QueryObject;
         private PointF m_CanvasMouseLocation;
+        private WireState m_WireState;
         private IGH_DocumentObject m_DocumentObject;
 
         public QueryObjectInteraction(GH_Canvas canvas, GH_CanvasMouseEvent mouseEvent, QueryObject queryObject) : base(canvas, mouseEvent)
@@ -52,9 +61,48 @@ namespace QuerySniffer.Components.Interactions
             return base.RespondToMouseMove(sender, e);
         }
 
+        public override GH_ObjectResponse RespondToKeyDown(GH_Canvas sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.ShiftKey:
+                    m_WireState = WireState.Add;
+                    break;
+                case Keys.ControlKey:
+                    m_WireState = WireState.Remove;
+                    break;
+            }
+            return base.RespondToKeyDown(sender, e);
+        }
+
+        public override GH_ObjectResponse RespondToKeyUp(GH_Canvas sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.ShiftKey:
+                case Keys.ControlKey:
+                    m_WireState = WireState.New;
+                    break;
+            }
+            return base.RespondToKeyUp(sender, e);
+        }
+
         public override GH_ObjectResponse RespondToMouseUp(GH_Canvas sender, GH_CanvasMouseEvent e)
         {
-            m_QueryObject.AddVolatileData(new GH_Path(0), m_QueryObject.VolatileData.DataCount, new GrasshopperObject(m_DocumentObject));
+            switch (m_WireState)
+            {
+                case WireState.New:
+                    m_QueryObject.VolatileData.Clear();
+                    m_QueryObject.AddVolatileData(new GH_Path(0), m_QueryObject.VolatileData.DataCount, new GrasshopperObject(m_DocumentObject));
+                    break;
+                case WireState.Add:
+                    m_QueryObject.AddVolatileData(new GH_Path(0), m_QueryObject.VolatileData.DataCount, new GrasshopperObject(m_DocumentObject));
+                    break;
+                case WireState.Remove:
+                    var item = m_QueryObject.VolatileData.AllData(true).FirstOrDefault(obj => (obj as GrasshopperObject).Value == m_DocumentObject);
+                    m_QueryObject.VolatileData.get_Branch(0).Remove(item);
+                    break;
+            }
 
             foreach (var param in m_QueryObject.Recipients)
                 param.ExpireSolution(true);
